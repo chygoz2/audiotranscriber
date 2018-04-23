186449750660855350086f83730cdcea7c089cf1
Travis CI <travis@travis-ci.org>
2018-04-23 20:50:04 +0000

    committed at b86a2e1 [ci skip]

--- push.sh
+++ push.sh
@@ -2,10 +2,11 @@
 rev=$(git rev-parse --short HEAD)
 git config --global user.email "travis@travis-ci.org"	
 git config --global user.name "Travis CI"	
-git clone -b master https://chygoz2:${AUTOBUILD_TOKEN}@chygoz2/dragonglass master	
-git clone -b prod https://chygoz2:${AUTOBUILD_TOKEN}@github.com/HNGInternship/HNGFun prod	
-yes | cp -rf master/profiles/* prod/profiles/
-yes | cp -rf master/answer* prod/
+git clone -b master https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass master	
+git clone -b prod https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass prod	
+# git clone -b master https://github.com/chygoz2/dragonglass master	
+# git clone -b prod https://github.com/chygoz2/dragonglass prod	
+yes | cp -rf master/* prod/
 cd prod
 git add .	
 git commit -m "committed at ${rev} [ci skip]"
