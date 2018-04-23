4d30a8f09cc4b46500873d53825bdf8a54e2b6dc
chygoz2 <chygoz2@gmail.com>
2018-04-23 21:05:12 +0100

    edit push.sh

--- push.sh
+++ push.sh
@@ -2,8 +2,10 @@
 rev=$(git rev-parse --short HEAD)
 git config --global user.email "travis@travis-ci.org"	
 git config --global user.name "Travis CI"	
-git clone -b master https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass master	
-git clone -b prod https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass prod	
+# git clone -b master https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass master	
+# git clone -b prod https://chygoz2:${AUTOBUILD_TOKEN}@github.com/chygoz2/dragonglass prod	
+git clone -b master https://github.com/chygoz2/dragonglass master	
+git clone -b prod https://github.com/chygoz2/dragonglass prod	
 yes | cp -rf master/* prod/
 cd prod
 git add .	
