7b2b6f04a4c8c4ebf9f4462472912c1e3de6be4b
Everistus Olumese <everistusolumese@gmail.com>
2018-04-24 18:26:23 +0100

    Add the functionality to upload files to s3

--- src/js/viewModels/home.js
+++ src/js/viewModels/home.js
@@ -63,7 +63,10 @@ define(['ojs/ojcore', 'knockout', 'jquery'],
                 var formData = new FormData();
                 formData.append("file", $file[0].files[0])
 
-
+                var credentials = {
+                    accessKeyId: 'AKIAJU3WLQLK2C4MIDLA',
+                    secretAccessKey: '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'
+                };
                 AWS.config.update(credentials);
                 AWS.config.region = 'us-east-2';
 
@@ -81,9 +84,9 @@ define(['ojs/ojcore', 'knockout', 'jquery'],
                         if (err) {
                             console.log(err)
                         } else {
-                            console.log(data);
-                            awsTranscribe(filename);
-                            googleTranscribe(filename);
+                            // console.log(data);
+                            awsTranscribe(data.Location);
+                            googleTranscribe(data.Location);
                         }
                     });
                 }
